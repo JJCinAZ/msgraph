5e563d4ddac71fb9e75ac6026805c1f87e7a82c2
Joseph Cracchiolo <jjc@leapscape.com>
2020-06-01 17:05:40 -0700

    Added ProfilePic example

---
+++ examples/profilePic.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/qeesung/image2ascii/convert"
+	"image"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jjcinaz/msgraph"
+	filecache "github.com/jjcinaz/msgraph/filecache"
+)
+
+func main() {
+	var (
+		err                              error
+		c                                *msgraph.Client
+		userid                           string
+		debugmode                        bool
+		tenantid, clientid, clientsecret string
+	)
+	tenantid = os.Getenv("AZURE_TENANTID")
+	clientid = os.Getenv("AZURE_CLIENTID")
+	clientsecret = os.Getenv("AZURE_CLIENTSECRET")
+	if len(tenantid) == 0 {
+		fmt.Println("Missing environment variable AZURE_TENANTID")
+	}
+	if len(clientid) == 0 {
+		fmt.Println("Missing environment variable AZURE_CLIENTID")
+	}
+	if len(clientsecret) == 0 {
+		fmt.Println("Missing environment variable AZURE_CLIENTKEY")
+	}
+	flag.StringVar(&userid, "u", "", "User Id or User Principal Name (email)")
+	flag.BoolVar(&debugmode, "debug", false, "enable debug mode")
+	flag.Parse()
+	c, err = msgraph.NewClient(context.Background(), tenantid, clientid, clientsecret,
+		[]string{"user.read", "Calendars.Read"}, filecache.New(), time.Minute)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+	if debugmode {
+		c.SetAPILogging(log.New(os.Stdout, "", 0))
+	}
+	asciiProfilePhoto(c, userid)
+}
+
+func asciiProfilePhoto(c *msgraph.Client, id string) {
+	var (
+		err error
+		img image.Image
+	)
+	img, _, err = c.GetUserPhoto(id)
+	if err == nil {
+		converter := convert.NewImageConverter()
+		fmt.Print(converter.Image2ASCIIString(img, &convert.DefaultOptions))
+	} else {
+		fmt.Println(err)
+	}
+}
